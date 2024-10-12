c8aece11174d68e0b831ad02814898db256ae7fb
expiol <128288350+expiol@users.noreply.github.com>
2024-10-12 15:22:45 +0800

    Update search_tool.py

--- Tools/search_tool.py
+++ Tools/search_tool.py
@@ -1,10 +1,23 @@
+import os
 from langchain_community.utilities import SerpAPIWrapper
 from langchain.tools import Tool
 
-# 加载 SERPAPI 搜索工具
-search = SerpAPIWrapper()
-search_tool = Tool.from_function(
-    func=search.run,
-    name="Search",
-    description="Used to search for information on the Internet through search engines"
-)
+serpapi_api_key = os.getenv("SERPAPI_API_KEY")
+
+if serpapi_api_key:
+    search = SerpAPIWrapper()
+    search_tool = Tool.from_function(
+        func=search.run,
+        name="Search",
+        description="Used to search for information on the Internet through search engines"
+    )
+else:
+    def unavailable_search(query: str) -> str:
+        return "未填入api，无法使用search工具"
+
+    search_tool = Tool.from_function(
+        func=unavailable_search,
+        name="Search",
+        description="未填入api，无法使用search工具"
+    )
+
